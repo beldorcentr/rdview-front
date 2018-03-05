d00a1b801cd1106034da6aee6db0a3bf5ec6e487
muravev.a <muravev.a@bdc.mtk.by>
2018-03-05 12:33:27 +0300

    Change auth client id

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'api',
   authorizationEndpoint: 'https://i.centr.by/oauth/token',
-  authorizationCliendId: '8a8dbd75e60440ab94c2da5911d88f7a'
+  authorizationCliendId: '8ty7TVwSULs0aOMp8KZBK8WMGGvaakos'
 };
