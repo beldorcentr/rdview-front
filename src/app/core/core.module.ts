cb4e38a017a16c184aff4627ded3559d618452f7
muravev.a <muravev.a@bdc.mtk.by>
2018-06-28 14:44:03 +0300

    Add header navigation

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SharedModule } from 'app/shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    RouterModule
   ],
   declarations: [
     HeaderComponent
